44b647d5b0884bb7ad350f37c1088144e315f538
Marcin <marcin080599@gmail.com>
2020-02-27 18:28:33 +0100

    adding a comment

--- venv/Scripts/index.py
+++ venv/Scripts/index.py
@@ -6,6 +6,8 @@ from Scripts import get_coordinates
 class iss:
 
     def jsonresponse(self , parameters):
+        #connect to api with coordinates sent
+        #if a connection is failed then it show error code
         try :
             response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
             return response.json()['response']
@@ -14,9 +16,9 @@ class iss:
 
 
     def parsejson(self,parameters):
-
+        # get json from API
         passtimes = self.jsonresponse(parameters)
-
+        #creating a table to put area from risetime section from json
         risetimes = []
 
         for d  in passtimes:
@@ -24,15 +26,19 @@ class iss:
             risetimes.append(time)
 
         times = []
-
+        #change format of date
         for rt in risetimes:
             time = datetime.fromtimestamp(rt)
             times.append(time)
             print(time)
 
+#initialize get_coordinates.py class
 coordinates = get_coordinates.getcord()
+#Get coordinates from the city using opencagedata API and assign the result  to parameters
 parameters = coordinates.parsecord()
+#initialize iss class
 issapi = iss()
+#Init a parsejson function to get a final dates
 issapi.parsejson(parameters)
 
 
